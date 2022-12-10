aa8e14fccf2ca983f3ec6b6cb86724b9b0814850
Andrei Voicu <andrei.voicu110@gmail.com>
2022-12-10 21:49:06 +0200

    Update

---
+++ components/combobox.tsx
@@ -0,0 +1,7 @@
+import styles from "styles/components/combobox.module.scss";
+
+const ComboBox = () => {
+  return <div className={styles["combobox"]}></div>;
+};
+
+export default ComboBox;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,28 +2,24 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { HelmetProvider } from "react-helmet-async";
 
-import { ContextProvider } from "context";
-
 import "styles/globals.scss";
 import styles from "styles/components/layout.module.scss";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ContextProvider>
-      <HelmetProvider>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>pITcasso</title>
-        </Head>
+    <HelmetProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>pITcasso</title>
+      </Head>
 
-        <div className={styles["app"]}>
-          <Component {...pageProps} />
-        </div>
-      </HelmetProvider>
-    </ContextProvider>
+      <div className={styles["app"]}>
+        <Component {...pageProps} />
+      </div>
+    </HelmetProvider>
   );
 };
 
