234fe065b1387226e88cd9f68a21db883dce77e8
Andrei Voicu <andrei.voicu110@gmail.com>
2022-12-10 14:25:56 +0200

    New

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import styles from "styles/pages/home.module.scss";
 
 const Home = () => {
   return (
-    <div className={styles["page"]}></div>
+    <div className={styles["page"]}>
+      
+    </div>
   );
 };
 
