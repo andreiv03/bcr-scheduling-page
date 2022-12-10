69b961248c7a68d0167a56e3a16d8d8f021c07e3
Kali <spinu11_2003@yahoo.ro>
2022-12-10 17:25:57 -0500

    Animations beta version

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -3,15 +3,17 @@ import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 const SearchBar: React.FC = () => {
   const [value, setValue] = useState("");
+  const [position,setPosition] = useState(false);
   function show(e: React.ChangeEvent<HTMLInputElement>) {
     console.log(value);
     setValue(e!.target!.value);
+    setPosition(true);
+    console.log(position)
   }
   return (
     <main className={styles["title-message"]}>
-      <p className={styles["search-bar"]}>Caută unitatea:</p>
-      <div className={styles["input-bar"]}>
-        {" "}
+     <p className={styles["search-bar"]}>Caută unitatea:</p> 
+     { position ?<div className={styles["input-bar"]}>
         <input
           className={styles["input-type"]}
           placeholder="Nume unitate / Adresa / Zona"
@@ -19,7 +21,15 @@ const SearchBar: React.FC = () => {
           value={value}
           onChange={show}
         />
-      </div>
+      </div> :<div className={styles["input-bar"]}>
+        <input
+          className={styles["input-type"]}
+          placeholder="Nume unitate / Adresa / Zona"
+          type="text"
+          value={value}
+          onChange={show}
+        />
+      </div>}
 <AiOutlineSearch className={styles["search-loop"]}/>
     </main>
   );
