3c74f1e921245ca26331efc8c5abdd335e46cf82
Kali <spinu11_2003@yahoo.ro>
2022-12-10 13:42:43 -0500

    First version of the FrontEndV1.2

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,4 +1,4 @@
-import styles from "styles/components/search-bar.module.scss";
+import styles from "./styles/components/search-bar.module.scss";
 import React, { useState } from "react";
 const SearchBar: React.FC = () => {
   const [value, setValue] = useState("");
