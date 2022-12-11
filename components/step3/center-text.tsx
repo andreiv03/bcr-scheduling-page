4c8ea6f1e5a1ccaebc09c694b1ac7eb5423f8918
Kali <spinu11_2003@yahoo.ro>
2022-12-11 00:12:48 -0500

    Calendar Page Desktop Part 1

--- components/step3/center-text.tsx
+++ components/step3/center-text.tsx
@@ -3,10 +3,10 @@ import styles from "styles/components/center-text.module.scss";
 const CenterTextStep3: React.FC = () => {
   return (
     <main className={styles["title-message"]}>
-      <p className={styles["header-step3"]}>In ce zi ne viziti?</p>
+      <p className={styles["header-step3"]}>In ce zi ne vizitezi?</p>
       <p className={styles["header-description-step3"]}>
       Alege data si intervalul orar pentru 
-vizita la "{"backend"}".
+vizita la {"backend"}/{"frontend"}.
       </p>
     </main>
   );
